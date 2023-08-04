435a39b6ce90d673fc9fa0cd7713bb9a29414781
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-08-04 23:01:13 +0900

    Gallery album title layout demo ver.1

--- src/routes/Gallery/AlbumThumb.jsx
+++ src/routes/Gallery/AlbumThumb.jsx
@@ -4,19 +4,21 @@ import { Link } from "react-router-dom";
 
 export default function AlbumThumb({ album }) {
   return (
-    <div className={styles.album}>
-      <div className={styles.wrapper}>
-        <Link to={`${album.id}`}>
-          <img
-            className={styles.image}
-            src={`${process.env.PUBLIC_URL}/assets/${album.thumbnail}`}
-            alt={"album thumbnail"}
-          />
-          <div className={styles.cover}></div>
-        </Link>
+    <>
+      <div className={styles.album}>
+        <div className={styles.wrapper}>
+          <Link to={`${album.id}`}>
+            <img
+              className={styles.image}
+              src={`${process.env.PUBLIC_URL}/assets/${album.thumbnail}`}
+              alt={"album thumbnail"}
+            />
+            <div className={styles.cover}></div>
+          </Link>
+          <div className={styles.title}>{album.title}</div>
+        </div>
       </div>
-      <span className={styles.title}>{album.title}</span>
-    </div>
+    </>
   );
 }
 
