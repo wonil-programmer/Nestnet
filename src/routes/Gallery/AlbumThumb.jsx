c32ff9501a75139652950a872307b5f280cf6761
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-08-04 23:39:01 +0900

    Gallery album title layout demo ver.2

--- src/routes/Gallery/AlbumThumb.jsx
+++ src/routes/Gallery/AlbumThumb.jsx
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 import styles from "./AlbumThumb.module.css";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 export default function AlbumThumb({ album }) {
   return (
@@ -13,9 +15,18 @@ export default function AlbumThumb({ album }) {
               src={`${process.env.PUBLIC_URL}/assets/${album.thumbnail}`}
               alt={"album thumbnail"}
             />
-            <div className={styles.cover}></div>
+            <div className={styles.cover}>
+              <div className={styles.title}>{album.title}</div>
+              <div className={styles.metadata}>
+                <div className={styles.visits}>
+                  <FontAwesomeIcon icon={faEye} /> <span>222</span>
+                </div>
+                <div className={styles.likes}>
+                  <FontAwesomeIcon icon={faHeart} /> <span>222</span>
+                </div>
+              </div>
+            </div>
           </Link>
-          <div className={styles.title}>{album.title}</div>
         </div>
       </div>
     </>
