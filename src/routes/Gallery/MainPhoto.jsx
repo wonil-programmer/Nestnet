fc2d674727d348d6d9669d645bd3644de0e82750
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-08-08 16:11:14 +0900

    Album mainphoto resizing

--- src/routes/Gallery/MainPhoto.jsx
+++ src/routes/Gallery/MainPhoto.jsx
@@ -1,35 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./MainPhoto.module.css";
 import { useImageSize } from "react-image-size";
 import { useRef } from "react";
 
 export default function MainPhoto({ mainImage }) {
   const imgSrc = `${process.env.PUBLIC_URL}/assets/${mainImage}`;
-  const reSize = useRef(false);
+  const [resize, setResize] = useState(false);
   const [dimensions, { isLoading, error }] = useImageSize(imgSrc);
 
-  // 종횡비(너비 / 폭)가 1.4(Album.jsx의 visualContent의 종횡비(올림))보다 작은 경우, 리사이징
-  // const handleSize = () => {
-  // }
+  // 기존 이미지의 높이가 760px((Album.jsx의 visualContent의 높이)보다 크고,
+  // 종횡비(너비/폭)가 1.3(Album.jsx의 visualContent의 종횡비(올림))보다 작은 경우 리사이징
   useEffect(() => {
-    if (dimensions) {
-      if (dimensions?.width / dimensions?.height < 1.4) {
-        console.log(reSize.current);
-        reSize.current = !reSize.current;
-        console.log("리사이징!");
-        console.log(reSize.current);
-      }
-    }
+    console.log(dimensions?.width / dimensions?.height);
+    console.log(dimensions?.height);
+    dimensions?.height > 760 && dimensions?.width / dimensions?.height < 1.3
+      ? setResize(true)
+      : setResize(false);
   }, [dimensions]);
-  useEffect(() => {
-    console.log("렌더링!!!");
-  });
+
   return (
     <>
       {isLoading ? (
         <h2>Loading...</h2>
       ) : (
-        <img className={styles.mainPhoto} src={imgSrc} alt="img__main" />
+        <img
+          className={resize ? styles.reSize : styles.sameSize}
+          src={imgSrc}
+          alt="img__main"
+        />
       )}
     </>
   );
