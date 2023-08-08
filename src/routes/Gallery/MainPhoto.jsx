53c4905a5080d374f9d1479b1c4203d56d98c1ca
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-08-08 23:09:44 +0900

    Gallery add json data

--- src/routes/Gallery/MainPhoto.jsx
+++ src/routes/Gallery/MainPhoto.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 import styles from "./MainPhoto.module.css";
 import { useImageSize } from "react-image-size";
-import { useRef } from "react";
 
 export default function MainPhoto({ mainImage }) {
   const imgSrc = `${process.env.PUBLIC_URL}/assets/${mainImage}`;
   const [resize, setResize] = useState(false);
   const [dimensions, { isLoading, error }] = useImageSize(imgSrc);
 
+  const visualContentHeight = 760;
+  const visualContentRatio = 1.3;
   // 기존 이미지의 높이가 760px((Album.jsx의 visualContent의 높이)보다 크고,
   // 종횡비(너비/폭)가 1.3(Album.jsx의 visualContent의 종횡비(올림))보다 작은 경우 리사이징
   useEffect(() => {
-    console.log(dimensions?.width / dimensions?.height);
-    console.log(dimensions?.height);
-    dimensions?.height > 760 && dimensions?.width / dimensions?.height < 1.3
+    dimensions?.height > visualContentHeight &&
+    dimensions?.width / dimensions?.height < visualContentRatio
       ? setResize(true)
       : setResize(false);
   }, [dimensions]);
