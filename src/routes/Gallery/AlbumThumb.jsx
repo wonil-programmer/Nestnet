7ba62528c1a25e22acbff04ba988208b2cfef632
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-08-09 19:53:22 +0900

    Gallery infinite scroll test

--- src/routes/Gallery/AlbumThumb.jsx
+++ src/routes/Gallery/AlbumThumb.jsx
@@ -7,30 +7,28 @@ import { faEye, faHeart } from "@fortawesome/free-solid-svg-icons";
 // album : 개별 앨범에 대한 정보
 export default function AlbumThumb({ album }) {
   return (
-    <>
-      <div className={styles.album}>
-        <div className={styles.wrapper}>
-          <Link to={`${album.id}`} state={{ data: album }}>
-            <img
-              className={styles.image}
-              src={`${process.env.PUBLIC_URL}/assets/${album.thumbnail}`}
-              alt={"album thumbnail"}
-            />
-            <div className={styles.cover}>
-              <div className={styles.title}>{album.title}</div>
-              <div className={styles.metadata}>
-                <div className={styles.visits}>
-                  <FontAwesomeIcon icon={faEye} /> <span>{album.visits}</span>
-                </div>
-                <div className={styles.likes}>
-                  <FontAwesomeIcon icon={faHeart} /> <span>{album.likes}</span>
-                </div>
+    <div className={styles.album}>
+      <div className={styles.wrapper}>
+        <Link to={`${album.id}`} state={{ data: album }}>
+          <img
+            className={styles.image}
+            src={`${process.env.PUBLIC_URL}/assets/${album.thumbnail}`}
+            alt={"album thumbnail"}
+          />
+          <div className={styles.cover}>
+            <div className={styles.title}>{album.title}</div>
+            <div className={styles.metadata}>
+              <div className={styles.visits}>
+                <FontAwesomeIcon icon={faEye} /> <span>{album.visits}</span>
+              </div>
+              <div className={styles.likes}>
+                <FontAwesomeIcon icon={faHeart} /> <span>{album.likes}</span>
               </div>
             </div>
-          </Link>
-        </div>
+          </div>
+        </Link>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/routes/Gallery/Gallery.jsx
+++ src/routes/Gallery/Gallery.jsx
@@ -1,13 +1,52 @@
 import AlbumThumb from "./AlbumThumb";
 import styles from "./Gallery.module.css";
 import Header from "../../components/Header";
-import useFetch from "../../hooks/useFetch";
+// import useFetch from "../../hooks/useFetch";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function Gallery() {
-  const { data: albums, isLoading } = useFetch(
-    "http://localhost:3001/galleries"
-  );
+  const [albums, setAlbums] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const limit = 5;
+  // const { data: albums, isLoading } = useFetch(
+  //   "http://localhost:3001/galleries",
+  //   offset,
+  //   limit
+  // );
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
 
+        const res = await fetch(
+          `http://localhost:3001/galleries?_start=${offset}&_limit=${limit}`
+        );
+        const data = await res.json();
+        setIsLoading(false);
+
+        setAlbums((prev) => [...prev, ...data]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [offset]);
+
+  useEffect(() => {
+    const handleScroll = (e) => {
+      const scrollHeight = e.target.documentElement.scrollHeight;
+      const currentHeight =
+        e.target.documentElement.scrollTop + window.innerHeight;
+      if (currentHeight + 1 >= scrollHeight) {
+        setOffset(offset + limit);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    console.log("로딩");
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [offset, isLoading]);
   return (
     <div className={styles.gallery}>
       <Header />
@@ -16,9 +55,10 @@ function Gallery() {
       ) : (
         <>
           <section className={styles.albums}>
-            {albums.map((album) => (
-              <AlbumThumb key={album.id} id={album.id} album={album} />
-            ))}
+            {albums &&
+              albums.map((album) => (
+                <AlbumThumb key={album.id} id={album.id} album={album} />
+              ))}
           </section>
         </>
       )}
