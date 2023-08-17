6b0c1af809bf92b8f577a58639e024ac04cca8ac
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-08-17 21:25:44 +0900

    Gallery infinite scroll  masonary layout

--- src/routes/Gallery/Gallery.jsx
+++ src/routes/Gallery/Gallery.jsx
@@ -3,54 +3,49 @@ import styles from "./Gallery.module.css";
 import Header from "../../components/Header";
 import { useEffect } from "react";
 import { useState } from "react";
+import Masonry from "react-masonry-css";
+import InfiniteScroll from "react-infinite-scroll-component";
+import fetchAlbums from "./fetchAlbums";
+import { Flex } from "@chakra-ui/react";
 
 function Gallery() {
   const [albums, setAlbums] = useState([]);
-  const [offset, setOffset] = useState(0);
+  // const [offset, setOffset] = useState(0);
+  // const limit = 5;
   const [isLoading, setIsLoading] = useState(false);
-  const limit = 5;
+
+  const [hasMore, setHasMore] = useState(false);
+  const [page, setPage] = useState(1);
+  const fetchNextAlbums =
+    ({ page, setAlbums, setPage, setHasMore }) =>
+    async () => {
+      const nextAlbums = await fetchAlbums({
+        setPage,
+        setHasMore,
+        page,
+      });
+
+      setAlbums((prev) => prev.concat(nextAlbums));
+    };
+
+  const initGallery = async () => {
+    const initAlbums = await fetchAlbums({ setPage, setHasMore });
+    console.log(initAlbums);
+    setAlbums(initAlbums);
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setIsLoading(true);
+    setIsLoading(true);
+    initGallery();
 
-        const res = await fetch(
-          `http://localhost:3001/galleries?_start=${offset}&_limit=${limit}`
-          // `http://192.168.0.25:8080/photo-post?offset=${offset}&limit=${limit}`,
-          // {
-          //   method: 'GET',
-          //   // headers: {
-          //   //   Authorization:
-          //   //     "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0MiIsImF1dGgiOiJHRU5FUkFMX01FTUJFUiIsImV4cCI6MTY5MjA4ODU0Mn0.29hJOnbb1YKz16NDQFOc8WZ_hDvLJ3BZLzhv4vrA1PEbyTux_bjiuLseiFP6klaI5N0hDBlwMolkTGj0uUo6EQ",
-          //   //   "Content-Type": "application/json",
-          //   // },
-          // }
-        );
-        const data = await res.json();
-        setIsLoading(false);
+    setIsLoading(false);
+  }, []);
 
-        setAlbums((prev) => [...prev, ...data]);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, [offset]);
-
-  useEffect(() => {
-    const handleScroll = (e) => {
-      const scrollHeight = e.target.documentElement.scrollHeight;
-      const currentHeight =
-        e.target.documentElement.scrollTop + window.innerHeight;
-      if (currentHeight + 1 >= scrollHeight) {
-        setOffset(offset + limit);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    console.log("로딩");
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [offset, isLoading]);
+  const breakpointColumnsObj = {
+    default: 3,
+    1500: 2,
+    800: 1,
+  };
 
   return (
     <div className={styles.gallery}>
@@ -58,15 +53,24 @@ function Gallery() {
       {isLoading ? (
         <h1>LOADING...</h1>
       ) : (
-        <>
-          <section className={styles.albums}>
+        <InfiniteScroll
+          dataLength={albums.length}
+          hasMore={hasMore}
+          next={fetchNextAlbums({
+            page,
+            setAlbums,
+            setPage,
+            setHasMore,
+          })}
+        >
+          <Flex as={Masonry} breakpointCols={breakpointColumnsObj}>
             {albums &&
               albums.map((album) => (
                 <GalleryThumb key={album.id} id={album.id} album={album} />
                 // <GalleryThumb key={album.postId} id={album.postId} album={album} />
               ))}
-          </section>
-        </>
+          </Flex>
+        </InfiniteScroll>
       )}
     </div>
   );
