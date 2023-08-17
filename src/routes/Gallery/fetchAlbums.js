b1a1c6ef672a67c55dcaf6824d142c6dc28b125b
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-08-17 21:24:38 +0900

    Gallery fetchAlbum function

---
+++ src/routes/Gallery/fetchAlbums.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const fetchAlbums = async ({ setPage, setHasMore, page = 1 }) => {
+  console.log(page);
+  const res = await fetch(`http://localhost:3001/galleries?_page=${page}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = res.json();
+  //   const data = await axios.get("http://localhost:3001/galleries", {
+  //     params: {
+  //       page,
+  //     },
+  //   });
+  setPage((prev) => prev + 1);
+  if (data.length < 10) {
+    setHasMore(false);
+  } else {
+    setHasMore(true);
+  }
+  return data;
+};
+
+export default fetchAlbums;
