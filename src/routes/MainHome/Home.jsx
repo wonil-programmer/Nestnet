2de7600cb4b3ccf4757dda7fe7872d5908341e7c
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-08-20 23:06:46 +0900

    Home remove extra files

--- src/routes/MainHome/Home.jsx
+++ src/routes/MainHome/Home.jsx
@@ -1,59 +1,11 @@
 import Header from "../../components/Header";
 import styles from "./Home.module.css";
-import Advantage from "./Advantages/Advantage";
-import Study from "./Study/Study";
-import Achievements from "./Achievements/Achievements";
 import { useEffect, useState } from "react";
 
-class SectAdv {
-  constructor(title, description, imgSrc) {
-    this.title = title;
-    this.description = description;
-    this.imgSrc = imgSrc;
-  }
-}
-const SectAdvs = [
-  new SectAdv(
-    "웬만한 건 다 있을 걸",
-    "소프트웨어학부 학술 동아리 중 가장 많은 시험 정보 보유!"
-  ),
-  new SectAdv(
-    "웬만한 건 다 있을 걸",
-    "소프트웨어학부 학술 동아리 중 가장 많은 시험 정보 보유!"
-  ),
-  new SectAdv(
-    "웬만한 건 다 있을 걸",
-    "소프트웨어학부 학술 동아리 중 가장 많은 시험 정보 보유!"
-  ),
-];
 function Home() {
-  const [advantages, setAdvantages] = useState([]);
-  useEffect(() => {
-    setAdvantages(SectAdvs);
-  }, []);
   return (
     <>
       <Header />
-      {advantages.map((ele, idx) => (
-        <section key={idx} className={styles.section}>
-          <Advantage
-            title={ele.title}
-            description={ele.description}
-            imgSrc={ele.imgSrc}
-          ></Advantage>
-        </section>
-      ))}
-      <section id="mainSection" className={styles.section}>
-        <p className={styles.mainTitle}>Study</p>
-        <Study />
-      </section>
-      <section id="mainSection" className={styles.section}>
-        <h2 className={styles.mainTitle}>History</h2>
-        <p className={styles.secDescription}>
-          더 나은 내일을 위해 정진하는 우리
-        </p>
-        <Achievements />
-      </section>
     </>
   );
 }
