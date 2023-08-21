f823bede3b6de41fe345a175e84a1de4302974c3
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-08-21 23:39:21 +0900

    Tailwind css set config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "home-maintitle": ["8.75rem", "10rem"],
+        "home-subtitle": "2.5rem",
+      },
+      colors: {
+        "home-primary": "#f43f5e",
+      },
+    },
   },
   plugins: [],
 };
