d20e06c66626a7ccd8bde56bcc13edf25c27db0c
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-08-22 18:12:11 +0900

    Home set layout

--- src/routes/MainHome/Home.jsx
+++ src/routes/MainHome/Home.jsx
@@ -1,11 +1,19 @@
 import Header from "../../components/Header";
-import styles from "./Home.module.css";
-import { useEffect, useState } from "react";
+import Footer from "./Footer/Footer";
+import MainView from "./MainView/MainView";
+import PhotoZone from "./PhotoZone/PhotoZone";
 
 function Home() {
   return (
     <>
       <Header />
+      <div class="">
+        <main>
+          <MainView></MainView>
+          <PhotoZone></PhotoZone>
+        </main>
+        <Footer></Footer>
+      </div>
     </>
   );
 }
