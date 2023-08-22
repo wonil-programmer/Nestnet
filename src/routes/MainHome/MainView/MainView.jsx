2c1dafa5fcd82f5e2ecf4b27bf3dbf5a8f6a12ae
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-08-22 18:12:49 +0900

    Home Mainview layout

---
+++ src/routes/MainHome/MainView/MainView.jsx
@@ -0,0 +1,21 @@
+export default function MainView() {
+  return (
+    <div class="mvArea relative w-screen h-screen overflow-hidden   ">
+      <div class="mvTitle absolute top-80 left-36">
+        <div class="text-home-maintitle text-white font-semibold">Nestnet</div>
+        <div class="text-home-subtitle text-white font-medium">
+          소프트웨어학부 1등 학술동아리
+        </div>
+      </div>
+      {/* <div class="mvImg absolute top-0 left-0 w-full h-full">
+        <div class="image-wrapper w-full h-full">
+          <img
+            class="w-full h-full"
+            src="C:\Users\wonza\Documents\nestnet-homepage\src\assets\images\a2.jpg"
+            alt="메인이미지"
+          />
+        </div>
+      </div> */}
+    </div>
+  );
+}
