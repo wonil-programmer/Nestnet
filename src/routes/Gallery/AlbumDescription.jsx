c765ecb93412174185ac890aac0c5daaf3fd487b
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-09-05 20:28:48 +0900

    Album album description tailwind css

--- src/routes/Gallery/AlbumDescription.jsx
+++ src/routes/Gallery/AlbumDescription.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import styles from "./AlbumDescription.module.css";
 import CommentForm from "../../components/Comments/CommentForm";
 import Comment from "../../components/Comments/Comment";
 import DownloadBtn from "../../components/Button/DownloadBtn";
@@ -8,17 +7,19 @@ export default function AlbumDescription({ albumData, mainImage }) {
   const [comments, setComments] = useState([]);
   return (
     <>
-      <div className={styles.comments}>
-        <div className={styles.top}>
-          <h3 className={styles.commentsCnt}>
+      <div>
+        <div class="top px-8 py-5 text-center flex justify-between border-b border-border-primary">
+          <h3 class="text-center leading-9">
             댓글
-            <span className={styles.commentsCntNum}>{albumData.comments}</span>
+            <span class="ml-2">{albumData.comments}</span>
           </h3>
           <DownloadBtn mainImage={mainImage} />
         </div>
         <div>
-          <div className={styles.body}>{albumData.body}</div>
-          <ul className={styles.comments__list}>
+          <div class="body w-full py-4 px-6 font-semibold overflow-auto">
+            {albumData.body}
+          </div>
+          <ul class="commentsList w-full py-4 px-12 max-h-80 overflow-auto">
             {/* api 호출 후 댓글 0개 일 때, 대체 텍스트 작성 */}
             {comments.map((item, index) => (
               <Comment comment={item} key={index} />
