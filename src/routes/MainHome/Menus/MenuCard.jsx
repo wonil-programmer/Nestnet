310c610dccc81cbb1ab3be34b181f258a144dfc3
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-09-05 20:09:34 +0900

    Home menucard link

--- src/routes/MainHome/Menus/MenuCard.jsx
+++ src/routes/MainHome/Menus/MenuCard.jsx
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function MenuCard({ mainTitle, subTitle, link }) {
   return (
-    <div class="w-menu h-64 p-10 bg-white rounded-2xl shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(228,95,132,0.15)]">
-      <div class="h-11 mb-2">
-        <span class="menuIcon mr-3">icon</span>
+    <Link to={`${link}`}>
+      <div class="w-menu h-64 p-10 bg-white rounded-2xl shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(228,95,132,0.15)] duration-300 hover:-translate-y-2 hover:ease-in-out hover:shadow-pink-300 hover:shadow-lg">
+        <div class="h-11 mb-2">
+          <span class="menuIcon mr-3">icon</span>
+        </div>
+        <div class="mainTitle mb-2">
+          <span class="text-2xl font-semibold">{mainTitle}</span>
+        </div>
+        <h2 class="subTitle h-20">{subTitle}</h2>
       </div>
-      <div class="mainTitle mb-2">
-        <span class="text-2xl font-semibold">{mainTitle}</span>
-      </div>
-      <h2 class="subTitle h-20">{subTitle}</h2>
-    </div>
+    </Link>
   );
 }
