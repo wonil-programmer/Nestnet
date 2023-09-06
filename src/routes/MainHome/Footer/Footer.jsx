806bd83b52c88caa2c6b868047216fa53246f466
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-09-06 21:50:51 +0900

    class -> className

--- src/routes/MainHome/Footer/Footer.jsx
+++ src/routes/MainHome/Footer/Footer.jsx
@@ -23,34 +23,36 @@ export default function Footer() {
   ];
 
   return (
-    <footer class="w-full my-3">
-      <div class="flex-col rounded shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)]">
-        <div class="flex-col px-60 py-8">
+    <footer className="w-full my-3">
+      <div className="flex-col rounded shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)]">
+        <div className="flex-col px-60 py-8">
           {/* 동아리 소개 */}
-          <div class="flex-col mb-8">
-            <div class="mb-4 font-semibold text-slate-800/90">소개</div>
-            <ul class="flex pl-2">
+          <div className="flex-col mb-8">
+            <div className="mb-4 font-semibold text-slate-800/90">소개</div>
+            <ul className="flex pl-2">
               {intros.map((intro) => (
                 <li
                   key={intro.title}
-                  class="mr-10 after:content-['|'] last:after:content-['']"
+                  className="mr-10 after:content-['|'] last:after:content-['']"
                 >
-                  <Link class="mr-10" to={intro.link} target="_blank">
-                    <span class="text-slate-700/90 text-sm">{intro.title}</span>
+                  <Link className="mr-10" to={intro.link} target="_blank">
+                    <span className="text-slate-700/90 text-sm">
+                      {intro.title}
+                    </span>
                   </Link>
                 </li>
               ))}
             </ul>
           </div>
           {/* 각종 링크들 */}
-          <div class="flex-col">
-            <div class="mb-4 font-semibold text-slate-700/90">
-              <ul class="flex">
+          <div className="flex-col">
+            <div className="mb-4 font-semibold text-slate-700/90">
+              <ul className="flex">
                 {Links.map((link) => (
                   <li key={link.title}>
-                    <Link class="mr-10" to={link.link} target="_blank">
+                    <Link className="mr-10" to={link.link} target="_blank">
                       <img
-                        class="w-10 h-12"
+                        className="w-10 h-12"
                         src={link.imgPath}
                         alt={link.title}
                       />
@@ -61,8 +63,8 @@ export default function Footer() {
             </div>
           </div>
           {/* 제작자 */}
-          <div class="flex-col text-xs text-slate-700/75">
-            <div class="mb-1">제작자</div>
+          <div className="flex-col text-xs text-slate-700/75">
+            <div className="mb-1">제작자</div>
             <ul>
               <li>
                 <span>Front. </span>김강민, 허원일
--- src/routes/MainHome/MainView/MainView.jsx
+++ src/routes/MainHome/MainView/MainView.jsx
@@ -12,27 +12,27 @@ export default function MainView() {
   }, []);
 
   return (
-    <div class="mvArea relative w-full h-screen">
-      <div class="mvImg absolute top-0 left-0 w-full h-full">
-        <div class="image-wrapper w-full h-full"></div>
+    <div className="mvArea relative w-full h-screen">
+      <div className="mvImg absolute top-0 left-0 w-full h-full">
+        <div className="image-wrapper w-full h-full"></div>
       </div>
-      <div class="mvTitle absolute top-80 left-36">
+      <div className="mvTitle absolute top-80 left-36">
         <div
-          class={`text-home-maintitle text-slate-800 font-semibold ${
+          className={`text-home-maintitle text-slate-800 font-semibold ${
             isLoaded ? "animate-fadeout" : "animate-fadein"
           }`}
         >
           {isLoaded ? "Who is the best?" : "Nestnet"}
         </div>
         <div
-          class={`text-home-subtitle text-slate-700 font-medium ${
+          className={`text-home-subtitle text-slate-700 font-medium ${
             isLoaded ? "" : "animate-fadein"
           }`}
         >
           {isLoaded ? "" : "소프트웨어학부 1등 학술동아리"}
         </div>
       </div>
-      <FaArrowDown class="absolute bottom-6 left-2/4 -translate-x-2/4 text-3xl text-stone-800 animate-bounce" />
+      <FaArrowDown className="absolute bottom-6 left-2/4 -translate-x-2/4 text-3xl text-stone-800 animate-bounce" />
     </div>
   );
 }
