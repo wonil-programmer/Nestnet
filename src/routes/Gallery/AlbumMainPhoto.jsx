fa879c8469c276e7a7d7d34223d81b382d2141c1
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-09-11 13:01:12 +0900

    [Opt]Albummain photo delete useImageSize library

--- src/routes/Gallery/AlbumMainPhoto.jsx
+++ src/routes/Gallery/AlbumMainPhoto.jsx
@@ -1,33 +1,23 @@
-import { useEffect, useState } from "react";
-import { useImageSize } from "react-image-size";
 import { MainPhotoContext } from "../../context/MainPhotoContext";
 import { useContext } from "react";
 
 export default function MainPhoto() {
   const { mainImage } = useContext(MainPhotoContext);
   const imgPath = `${process.env.PUBLIC_URL}/assets/${mainImage}`;
-  const [dimensions, { isLoading, error }] = useImageSize(imgPath);
-  const visualContentHeight = 760;
-  const visualContentRatio = 1.3;
 
+  window.addEventListener("load", function (event) {
+    console.log("All resources finished loading!");
+  });
   return (
     <>
-      {isLoading ? (
-        <div className="w-11 h-11 bg-slate-400"></div>
-      ) : (
-        <img
-          // 기존 이미지의 높이가 760px((Album.jsx의 visualContent의 높이)보다 크고,
-          // 종횡비(너비/폭)가 1.3(Album.jsx의 visualContent의 종횡비(올림))보다 작은 경우 리사이징
-          className={`${
-            dimensions?.height > visualContentHeight &&
-            dimensions?.width / dimensions?.height < visualContentRatio
-              ? "w-2/5"
-              : "w-4/5"
-          } rounded-3xl m-auto my-6 shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px]`}
-          src={imgPath}
-          alt="img__main"
-        />
-      )}
+      {
+        <div
+          className="w-4/5 h-5/6 rounded-3xl m-auto my-6  bg-center bg-contain bg-no-repeat "
+          style={{
+            backgroundImage: `url(${imgPath})`,
+          }}
+        ></div>
+      }
     </>
   );
 }
