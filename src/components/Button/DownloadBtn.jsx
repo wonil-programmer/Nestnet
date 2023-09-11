b25c6542da3c0ddaec8a67bc8e20ec530264aed4
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-09-11 20:47:27 +0900

    Album download button change styles

--- src/components/Button/DownloadBtn.jsx
+++ src/components/Button/DownloadBtn.jsx
@@ -1,19 +1,17 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload } from "@fortawesome/free-solid-svg-icons";
-import styles from "./DownloadBtn.module.css";
+import { FiDownload } from "react-icons/fi";
 import { MainPhotoContext } from "../../context/MainPhotoContext";
 import { useContext } from "react";
 
 export default function DownloadBtn() {
   const { mainImage } = useContext(MainPhotoContext);
 
-  const downldPath = `${process.env.PUBLIC_URL}/assets/${mainImage}`;
+  const downloadPath = `${process.env.PUBLIC_URL}/assets/${mainImage}`;
 
   return (
-    <a download={mainImage} href={downldPath}>
-      <div className={styles.btnDownld}>
-        <FontAwesomeIcon className={styles.iconDownld} icon={faDownload} />
-      </div>
+    <a download={mainImage} href={downloadPath}>
+      <button className="flex flex-col items-center box-content p-1 w-[2.3rem] h-[2.3rem] mr-1 rounded-full hover:bg-gray-200 hover:scale-105 duration-300">
+        <FiDownload className="text-3xl" />
+      </button>
     </a>
   );
 }
