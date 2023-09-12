798fdc65228fe82e77fbd32947fe12f7b8d2f6f9
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-09-12 19:57:00 +0900

    AlbumMainPhoto change file name to PhotoSelected

--- src/routes/Gallery/PhotoSelected.jsx
+++ src/routes/Gallery/PhotoSelected.jsx
@@ -1,7 +1,7 @@
 import { MainPhotoContext } from "../../context/MainPhotoContext";
 import { useContext } from "react";
 
-export default function MainPhoto() {
+const PhotoSelected = () => {
   const { mainImage } = useContext(MainPhotoContext);
   const imgPath = `${process.env.PUBLIC_URL}/assets/${mainImage}`;
 
@@ -9,7 +9,7 @@ export default function MainPhoto() {
     <>
       {
         <div
-          className="w-4/5 h-5/6 rounded-3xl m-auto my-6  bg-center bg-contain bg-no-repeat"
+          className="w-5/6 h-5/6 rounded-3xl m-auto my-6  bg-center bg-contain bg-no-repeat"
           style={{
             backgroundImage: `url(${imgPath})`,
           }}
@@ -17,4 +17,6 @@ export default function MainPhoto() {
       }
     </>
   );
-}
+};
+
+export default PhotoSelected;
