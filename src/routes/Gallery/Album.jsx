29c42d470886716a7c68ee9c129922fc51a9f32d
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-10-14 22:07:09 +0900

    design: 사진게시판 삭제 및 수정 버튼 추가, 삭제모달창 추가

--- src/routes/Gallery/Album.jsx
+++ src/routes/Gallery/Album.jsx
@@ -4,8 +4,12 @@ import AsidePhotos from "./AsidePhotos";
 import SelectedPhoto from "./SelectedPhoto";
 import SideBar from "../../components/SideBar";
 import { useState, useEffect } from "react";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import { Dialog, DialogContent, Button, IconButton } from "@mui/material";
+import { FiEdit3 } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
+import { TiDelete } from "react-icons/ti";
 import axios from "axios";
 import {
   setPhotoInfo,
@@ -36,6 +40,10 @@ const Album = () => {
   const commentDispatch = useDispatch();
 
   const [isLoading, setIsLoading] = useState(true);
+
+  const navigate = useNavigate();
+  // 모달창 가시여부
+  const [modalVisible, setModalVisible] = useState(false);
 
   // 실제 앨범 api 호출
   // useEffect(() => {
@@ -86,6 +94,27 @@ const Album = () => {
       ) : (
         <div className="wrapper relative top-[4.6rem] h-full flex bg-home-background">
           <SideBar className="fixed top-48" />
+          <div className="fixed flex bottom-[3rem] left-[4rem] w-[10rem] h-[4rem]">
+            <Button
+              style={{ height: "48px", margin: "6px" }}
+              variant="outlined"
+              color="error"
+              onClick={() => {
+                setModalVisible(true);
+              }}
+            >
+              삭제
+            </Button>
+            <Button
+              style={{ height: "48px", margin: "6px" }}
+              variant="outlined"
+              onClick={() => {
+                navigate(`/edit-board/${postId}`);
+              }}
+            >
+              수정
+            </Button>
+          </div>
           <div className="ctrView flex flex-col m-auto">
             <div className="relative -top-8 w-album-visWth h-screen  flex flex-col justify-center">
               <SelectedPhoto />
@@ -112,6 +141,54 @@ const Album = () => {
           <AsidePhotos photos={albumStates.photoInfo} />
         </div>
       )}
+      <Dialog open={modalVisible}>
+        <DialogContent className={"relative w-[23rem] h-[10rem]"}>
+          <IconButton
+            style={{ position: "absolute", top: "0", right: "0" }}
+            onClick={() => setModalVisible(false)}
+          >
+            <TiDelete />
+          </IconButton>
+          <div className="modal">
+            <div className="modal-message mt-6 my-5 mx-4">
+              해당 게시물을 정말 삭제하시겠습니까?
+            </div>
+            <div
+              className="modal-button"
+              style={{
+                marginTop: "24px",
+                display: "flex",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <Button
+                className={"w-[4.5rem]"}
+                variant="outlined"
+                color="error"
+                onClick={async () => {
+                  setModalVisible(false);
+                  axios.delete(``, {
+                    postId: postId,
+                  });
+                  alert("게시물이 성공적으로 삭제되었습니다.");
+                  window.location.href = "/gallery";
+                }}
+              >
+                예
+              </Button>
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={() => {
+                  setModalVisible(false);
+                }}
+              >
+                아니오
+              </Button>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
       {/* </div> */}
     </>
   );
