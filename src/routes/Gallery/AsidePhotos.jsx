53476a02bbb636e1e2143cf62f98ed470c254657
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-17 23:52:44 +0900

    comment: 컴포넌트 설명 주석 추가

--- src/routes/Gallery/AsidePhotos.jsx
+++ src/routes/Gallery/AsidePhotos.jsx
@@ -1,6 +1,11 @@
 import AsidePhoto from "./AsidePhoto";
 import { memo } from "react";
 
+/**
+ *
+ * @param {photos} - Album.jsx로부터 가져온 앨범 내 사진 정보
+ * @returns
+ */
 const AsidePhotos = ({ photos }) => {
   return (
     <div className="w-56 h-[calc(100vh-4.6rem)] fixed right-0  overflow-y-scroll">
