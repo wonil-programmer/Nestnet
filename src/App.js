670b5367278f44a3ea7be91a8d2aa3f75a57ec9d
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-18 21:59:48 +0900

    refactor: 앨범 내 댓글 컨텍스트 추가

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import Album from "./routes/Gallery/Album";
 import Home from "./routes/MainHome/Home";
 import Gallery from "./routes/Gallery/Gallery";
 import GalleryBoardPostForm from "./routes/Gallery/GalleryBoardPostForm";
+import { CommentContext } from "./context/CommentContext";
+import { useState } from "react";
 
 function App() {
+  const [comments, setComments] = useState([]);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<Home />} />
         <Route path={`gallery`} element={<Gallery />} />
-        <Route path={"gallery/:postId"} element={<Album />} />
+        <CommentContext.Provider value={(comments, setComments)}>
+          <Route path={"gallery/:postId"} element={<Album />} />
+        </CommentContext.Provider>
         <Route path={"gallery/form"} element={<GalleryBoardPostForm />} />
         <Route
           path={"gallery/:postId/edit"}
