0c738a885eb344ac4ee83427497b324db35d6d2b
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-11-19 15:11:07 +0900

    refactor: 앨범 좋아요 상태 전달 방법 변경
    props -> context

--- src/components/Button/LikeBtn.jsx
+++ src/components/Button/LikeBtn.jsx
@@ -1,59 +1,45 @@
 import { FaHeart } from "react-icons/fa";
+import { useContext } from "react";
 import { useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { setIsMemberLiked } from "../../routes/Gallery/albumReducer";
+import { IsAlbumLikeContext } from "../../context/IsAlbumLikeContext";
+import axios from "axios";
 
-// 좋아요 여부를 받아와 처리
 const LikeBtn = () => {
-  const isMemberLiked = useSelector((state) => state.album.isMemberLiked);
-  const likeDispatch = useDispatch();
-
   const { postId } = useParams();
-  // 좋아요 클릭시, 서버에 저장된 사용자의 좋아요 여부에 따른 로직 처리
+  const { IsAlbumLike, setIsAlbumLike } = useContext(IsAlbumLikeContext);
+
+  const sendRequest = (url, onSuccess) => {
+    axios
+      .put(url, null, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          onSuccess();
+        }
+      })
+      .catch((error) => {
+        console.error("네트워크 오류:", error);
+      });
+  };
+
   const toggleLike = () => {
-    // // 좋아요 취소를 누른 경우
-    if (isMemberLiked) {
-      // fetch(`~/${postId / dislike}`, {
-      fetch(`http://localhost:3002/${postId}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => {
-          if (res.ok) {
-            console.log("좋아요 취소 요청 성공");
-            likeDispatch(setIsMemberLiked(!isMemberLiked));
-          }
-        })
-        .catch((error) => {
-          console.error("네트워크 오류:", error);
-        });
-    }
-    // // 좋아요를 누른 경우
-    else {
-      // fetch(`~/${postId / like}`, {
-      fetch(`http://localhost:3002/${postId}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => {
-          if (res.ok) {
-            console.log("좋아요 요청 성공");
-            likeDispatch(setIsMemberLiked(!isMemberLiked));
-          }
-        })
-        .catch((error) => {
-          console.error("네트워크 오류:", error);
-        });
+    if (IsAlbumLike) {
+      const dislikeUrl = `${process.env.REACT_APP_SERVER}/${postId}/dislike`;
+      sendRequest(dislikeUrl, () => setIsAlbumLike(!IsAlbumLike));
+    } else {
+      const likeUrl = `${process.env.REACT_APP_SERVER}/${postId}/like`;
+      sendRequest(likeUrl, () => {
+        setIsAlbumLike(!IsAlbumLike);
+      });
     }
   };
 
   return (
     <div className="mt-1.5 mr-3 ml-5 mb-1">
-      {isMemberLiked ? (
+      {IsAlbumLike ? (
         <FaHeart
           className="text-[2.3rem] text-red-400 ease-in-out cursor-pointer duration-300 hover:scale-110"
           onClick={toggleLike}
---
+++ src/context/IsAlbumLikeContext.js
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+
+export const IsAlbumLikeContext = createContext(null);
