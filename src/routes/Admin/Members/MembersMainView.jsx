b651a8a2867e15a346fea932f6d315be31a93ad5
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-12-31 16:55:50 +0900

    style: 관리자페이지 회원관리 스타일 수정

--- src/routes/Admin/Members/MembersMainView.jsx
+++ src/routes/Admin/Members/MembersMainView.jsx
@@ -1,13 +1,25 @@
 import MemberList from "./MemberList";
+import SignupList from "./SignupList";
 
 const MembersMainView = () => {
   return (
     <>
-      <section className="w-full h-screen pt-20 pl-[14rem]">
-        <div className="mainView w-full h-full px-9 py-6">
-          <MemberList></MemberList>
+      <div className="w-full pt-20 pl-[14rem]">
+        <div className="mainView flex flex-row justify-evenly w-full h-full px-9 py-6">
+          <section className="memberList flex flex-col">
+            <h1 className="mb-4 text-lg font-semibold text-[#111111]">
+              동아리원
+            </h1>
+            <MemberList></MemberList>
+          </section>
+          <section className="signupReqList flex flex-col">
+            <h1 className="mb-4 text-lg font-semibold text-[#111111]">
+              회원가입 요청
+            </h1>
+            <SignupList></SignupList>
+          </section>
         </div>
-      </section>
+      </div>
     </>
   );
 };
