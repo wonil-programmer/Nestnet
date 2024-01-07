75bbb9a5c497d7d58063842ccfc4236a9c691c85
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-07 23:22:17 +0900

    style: 앨범 썸네일 호버시 데이터 표시로 변경

--- src/routes/Gallery/Thumbnail.jsx
+++ src/routes/Gallery/Thumbnail.jsx
@@ -1,26 +1,28 @@
-import { memo, forwardRef } from "react";
+import { memo, forwardRef, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faHeart } from "@fortawesome/free-solid-svg-icons";
 
-/**
- *
- * @param {album} album - Gallery.jsx로부터 넘어온 하나의 앨범에 대한 객체
- * @returns
- */
 const Thumbnail = forwardRef(({ album }, ref) => {
-  const imageRootPath = `${process.env.REACT_APP_SERVER}/image`;
-  let saveFilePath = album.saveFilePath;
-  let saveFileName = album.saveFileName;
-  // let thumbnailPath = imageRootPath + "/" + saveFilePath + "/" + saveFileName;
-  let thumbnailPath = saveFilePath + "/" + saveFileName;
+  const [thumbnailPath, setThumbnailPath] = useState("");
+  const getImagePath = (album) => {
+    const imageRootPath = `${process.env.REACT_APP_SERVER}/image`;
+    let saveFilePath = album.saveFilePath;
+    let saveFileName = album.saveFileName;
+    // let thumbnailPath = imageRootPath + "/" + saveFilePath + "/" + saveFileName;
+    // test: json-server
+    let thumbnailPath = saveFilePath + "/" + saveFileName;
+
+    return thumbnailPath;
+  };
+
+  useEffect(() => {
+    setThumbnailPath(getImagePath(album));
+  }, [album]);
 
   return (
     <>
-      <div
-        className="pt-0 px-2 pb-6 min-w-[22.65rem] max-w-[22.65rem]"
-        ref={ref}
-      >
+      <div className="m-4 min-w-[22.65rem] max-w-[22.65rem] h-min" ref={ref}>
         <Link
           to={`${album.postId}`}
           state={{
@@ -30,23 +32,25 @@ const Thumbnail = forwardRef(({ album }, ref) => {
             likeCount: album.likeCount,
           }}
         >
-          <div className="relative flex-col rounded-2xl overflow-hidden duration-300 hover:cursor-zoom-in hover:-translate-y-2 hover:ease-in-out shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] hover:shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
+          <div className="relative album flex flex-col w-full h-min rounded-xl overflow-hidden shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] hover:shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
             <img
-              className="w-full max-w-full brightness-95"
-              // src={thumbnailPath}
-              src={`/${thumbnailPath}`}
-              alt={"album thumbnail"}
+              src={getImagePath(album)}
+              alt="thumbnail"
+              className="rounded-xl"
             />
-            <div className="title mt-3 mb-0 text-slate-800 text-2xl text-center font-bold">
-              {album.title}
-            </div>
-            <div className="metadata flex justify-center my-4 text-[#666666]">
-              <div className="visits mr-2">
-                <FontAwesomeIcon icon={faEye} /> <span>{album.viewCount}</span>
+            <div className="absolute top-0 left-0 w-full h-full rounded-xl hover:bg-black hover:opacity-80 opacity-0 duration-300 cursor-zoom-in">
+              <div className="mt-6 mx-6 text-center text-white text-2xl font-bold">
+                {album.title}
               </div>
-              <div className="likes ml-2">
-                <FontAwesomeIcon icon={faHeart} />{" "}
-                <span>{album.likeCount}</span>
+              <div className="metadata absolute right-0 bottom-0 m-5 mr-6 text-[#666666]">
+                <span className="visits mr-2">
+                  <FontAwesomeIcon icon={faEye} />{" "}
+                  <span>{album.viewCount}</span>
+                </span>
+                <span className="likes ml-2">
+                  <FontAwesomeIcon icon={faHeart} />{" "}
+                  <span>{album.likeCount}</span>
+                </span>
               </div>
             </div>
           </div>
