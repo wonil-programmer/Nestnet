e622ff7ef82ee24f270b41d75068f01f96153d98
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-09 17:17:31 +0900

    refactor: 사진 확대 버튼에서 더블클릭 이벤트로 변경

--- src/routes/Gallery/SelectedPhoto.jsx
+++ src/routes/Gallery/SelectedPhoto.jsx
@@ -1,41 +1,22 @@
 import { useRef } from "react";
 import { memo } from "react";
-import { MdZoomOutMap } from "react-icons/md";
+import { triggerFullscreen } from "../../utils/triggerFullScreen";
 
 const SelectedPhoto = ({ selectedPhoto }) => {
   const selectedPhotoRef = useRef();
   // 각 브라우저에 대한 처리
   const triggerFull = () => {
-    if (selectedPhotoRef.current.requestFullscreen) {
-      selectedPhotoRef.current.requestFullscreen();
-    }
-    if (selectedPhotoRef.current.webkitRequestFullscreen) {
-      selectedPhotoRef.current.webkitRequestFullscreen();
-    }
-    if (selectedPhotoRef.current.mozRequestFullScreen) {
-      selectedPhotoRef.current.mozRequestFullScreen();
-    }
-    if (selectedPhotoRef.current.msRequestFullscreen) {
-      selectedPhotoRef.current.msRequestFullscreen();
-    }
+    triggerFullscreen(selectedPhotoRef.current);
   };
 
   return (
     <>
-      <div
-        className={
-          "relative max-w-max h-max m-auto my-6 text-center rounded-xl overflow-hidden shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]"
-        }
-      >
-        <img src={selectedPhoto} alt="thumbnail" ref={selectedPhotoRef} />
-        <button onClick={triggerFull} className={"absolute right-8 bottom-8"}>
-          <MdZoomOutMap
-            className={
-              "text-2xl text-stone-500 cursor-pointer hover:scale-105 hover:text-stone-600 duration-200 ease-in-out"
-            }
-          />
-        </button>
-      </div>
+      <img
+        src={selectedPhoto}
+        alt="thumbnail"
+        ref={selectedPhotoRef}
+        onDoubleClick={triggerFull}
+      />
     </>
   );
 };
---
+++ src/utils/triggerFullScreen.js
@@ -0,0 +1,11 @@
+export const triggerFullscreen = (element) => {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+};
