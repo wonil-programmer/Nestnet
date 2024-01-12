daf25f5791fa625ce44b7729b3b92ae42ac6d656
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-12 15:39:49 +0900

    feat: 스크롤 초기화 함수 추가

--- src/App.js
+++ src/App.js
@@ -5,36 +5,31 @@ import Gallery from "./routes/Gallery/Gallery";
 import GalleryBoardPostForm from "./routes/Gallery/GalleryBoardPostForm";
 import AdminHome from "./routes/Admin/AdminHome";
 import MembersMainView from "./routes/Admin/Members/MembersMainView";
-import { CommentsContext } from "./context/CommentsContext";
 import Header from "./components/Header";
 import AdminNavBar from "./routes/Admin/AdminNavbar";
-import { useState } from "react";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
-  const [comments, setComments] = useState([]);
-
   return (
     <>
       <BrowserRouter>
-        <CommentsContext.Provider value={{ comments, setComments }}>
-          <Routes>
-            <Route path={"/"} element={<Header />}>
-              <Route path={"/"} element={<Home />} />
-              <Route path={`gallery`} element={<Gallery />} />
-              <Route path={"gallery/:postId"} element={<Album />} />
-              <Route path={"gallery/form"} element={<GalleryBoardPostForm />} />
-              <Route
-                path={"gallery/:postId/edit"}
-                element={<GalleryBoardPostForm isModifying={true} />}
-              />
-            </Route>
-
-            <Route path={"admin/"} element={<AdminNavBar />}>
-              <Route path={"home"} element={<AdminHome />} />
-              <Route path={"users"} element={<MembersMainView />} />
-            </Route>
-          </Routes>
-        </CommentsContext.Provider>
+        <ScrollToTop />
+        <Routes>
+          <Route path={"/"} element={<Header />}>
+            <Route path={"/"} element={<Home />} />
+            <Route path={`gallery`} element={<Gallery />} />
+            <Route path={"gallery/:postId"} element={<Album />} />
+            <Route path={"gallery/form"} element={<GalleryBoardPostForm />} />
+            <Route
+              path={"gallery/:postId/edit"}
+              element={<GalleryBoardPostForm isModifying={true} />}
+            />
+          </Route>
+          <Route path={"admin/"} element={<AdminNavBar />}>
+            <Route path={"home"} element={<AdminHome />} />
+            <Route path={"users"} element={<MembersMainView />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </>
   );
