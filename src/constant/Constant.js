60d502dee2481c57db9f859f099d25810f0f5634
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-13 15:40:22 +0900

    fix: 서버로 보내는 권한명을 영문에서 한글로 변경하는 상수 추가

--- src/constant/Constant.js
+++ src/constant/Constant.js
@@ -3,7 +3,7 @@ export const AVAIL_AUTHORITY = [
   "부회장",
   "관리자",
   "재학생",
-  "휴학",
+  "휴학생",
   "졸업생",
 ];
 
@@ -11,10 +11,22 @@ export const AUTHORITY_ENG_TO_KOR = Object.freeze({
   PRESIDENT: "회장",
   VICE_PRESIDENT: "부회장",
   MANAGER: "관리자",
-  GENERAL_MEMBER: "일반",
-  ON_LEAVE_MEMBER: "휴학",
-  GRADUATED_MEMBER: "졸업",
+  GENERAL_MEMBER: "재학생",
+  ON_LEAVE_MEMBER: "휴학생",
+  GRADUATED_MEMBER: "졸업생",
   WAITING_FOR_APPROVAL: "승인대기",
   WITHDRAWN_MEMBER: "탈퇴",
-  UNKNOWN: "알 수 없음",
+  // UNKNOWN: "알수없음",
+});
+
+export const AUTHORITY_KOR_TO_ENG = Object.freeze({
+  회장: "PRESIDENT",
+  부회장: "VICE_PRESIDENT",
+  관리자: "MANAGER",
+  재학생: "GENERAL_MEMBER",
+  휴학생: "ON_LEAVE_MEMBER",
+  졸업생: "GRADUATED_MEMBER",
+  승인대기: "WAITING_FOR_APPROVAL",
+  탈퇴: "WITHDRAWN_MEMBER",
+  // 알수없음: "UNKNOWN",
 });
