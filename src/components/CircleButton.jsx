5aed4497fa0e76457ea0f2a915afd216743ec9b2
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-13 21:23:46 +0900

    feat: 버튼 비활성화 속성 추가

--- src/components/CircleButton.jsx
+++ src/components/CircleButton.jsx
@@ -1,9 +1,10 @@
-const CircleButton = ({ onClick, content }) => {
+const CircleButton = ({ onClick, content, disabled = false }) => {
   return (
     <>
       <button
         onClick={onClick}
         className="flex flex-col items-center box-content p-2 w-[2.3rem] h-[2.3rem] rounded-full bg-gray-200 hover:bg-gray-300 duration-300"
+        disabled={disabled}
       >
         {content}
       </button>
