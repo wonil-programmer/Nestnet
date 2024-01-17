3961de02b062810a1554444071c7b738118f9ed5
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-17 21:36:08 +0900

    design: 홈화면 메뉴 디자인 변경

--- src/routes/MainHome/Menus/MenuCard.jsx
+++ src/routes/MainHome/Menus/MenuCard.jsx
@@ -1,16 +1,25 @@
 import { Link } from "react-router-dom";
 
-export default function MenuCard({ mainTitle, subTitle, link }) {
+export default function MenuCard({ imgSrc, mainTitle, subTitle, link }) {
   return (
     <Link to={`${link}`}>
-      <div className="w-menu h-64 p-10 bg-white rounded-2xl shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(228,95,132,0.15)] duration-300 hover:-translate-y-2 hover:ease-in-out hover:shadow-pink-300 hover:shadow-lg">
-        <div className="h-11 mb-2">
-          <span className="menuIcon mr-3">icon</span>
+      <div
+        className={`flex flex-col w-[22rem] h-[30rem] bg-white rounded-2xl overflow-hidden 
+      shadow-[0_8px_30px_rgb(0,0,0,0.12)] duration-300 hover:-translate-y-2 hover:ease-in-out hover:shadow-lg`}
+      >
+        <div className="w-full h-1/2">
+          <img
+            className="w-full h-full object-cover brightness-90"
+            src={`${imgSrc}`}
+            alt="servicePhoto"
+          />
         </div>
-        <div className="mainTitle mb-2">
-          <span className="text-2xl font-semibold">{mainTitle}</span>
+        <div className="w-full h-1/2 p-8 text-black">
+          <div className="mainTitle mb-4">
+            <span className="text-2xl font-semibold">{mainTitle}</span>
+          </div>
+          <h2 className="subTitle h-20">{subTitle}</h2>
         </div>
-        <h2 className="subTitle h-20">{subTitle}</h2>
       </div>
     </Link>
   );
--- src/routes/MainHome/Menus/Menus.jsx
+++ src/routes/MainHome/Menus/Menus.jsx
@@ -2,8 +2,8 @@ import MenuCard from "./MenuCard";
 
 export default function Menus() {
   class Service {
-    constructor(icon, mainTitle, subTitle, link) {
-      this.icon = icon;
+    constructor(imgSrc, mainTitle, subTitle, link) {
+      this.imgSrc = imgSrc;
       this.mainTitle = mainTitle;
       this.subTitle = subTitle;
       this.link = link;
@@ -11,68 +11,36 @@ export default function Menus() {
   }
   const Services = [
     new Service(
-      "",
+      "assets/unified_board.jpg",
       "통합 게시판",
       "자유, 진로, 개발에 관련하여 의견을 공유할 수 있는 게시판",
       ""
     ),
     new Service(
-      "",
+      "assets/photo_board.jpg",
       "사진 게시판",
       "동아리 활동의 매 순간을 사진으로 기록하여 저장한 공간",
       "gallery"
     ),
     new Service(
-      "",
-      "족보",
-      "전공, 교양 할 것 없이 수많은 과목들의 기출문제를 보관하는 곳",
-      ""
-    ),
-    new Service(
-      "",
-      "통합 게시판",
-      "자유, 진로, 개발에 관련하여 의견을 공유할 수 있는 게시판",
-      ""
-    ),
-    new Service(
-      "",
-      "사진 게시판",
-      "동아리 활동의 매 순간을 사진으로 기록하여 저장한 공간",
-      "gallery"
-    ),
-    new Service(
-      "",
-      "족보",
-      "전공, 교양 할 것 없이 수많은 과목들의 기출문제를 보관하는 곳",
-      ""
-    ),
-    new Service(
-      "",
-      "통합 게시판",
-      "자유, 진로, 개발에 관련하여 의견을 공유할 수 있는 게시판",
-      ""
-    ),
-    new Service(
-      "",
-      "사진 게시판",
-      "동아리 활동의 매 순간을 사진으로 기록하여 저장한 공간",
-      "gallery"
-    ),
-    new Service(
-      "",
+      "assets/info_board.jpg",
       "족보",
       "전공, 교양 할 것 없이 수많은 과목들의 기출문제를 보관하는 곳",
       ""
     ),
   ];
+
   return (
-    <section class="w-full min-h-screen h-min pb-12">
-      <div class="text-5xl text-center font-semibold mt-32 mb-4">서비스</div>
-      <div class="menuContent h-full">
-        <ul class="menuList w-menus mx-auto flex flex-wrap">
-          {Services.map((service) => (
-            <li class="h-64 mt-6 px-5">
+    <section className="w-full min-h-screen h-min pb-12 bg-home-secondary/10">
+      <div className="text-5xl text-center  font-semibold mt-[8rem] mb-6 text-gray-700">
+        서비스
+      </div>
+      <div className="menuContent h-full">
+        <ul className="menuList w-menus mx-auto flex flex-wrap">
+          {Services.map((service, idx) => (
+            <li key={idx} className="h-64 mt-6 px-5">
               <MenuCard
+                imgSrc={service.imgSrc}
                 mainTitle={service.mainTitle}
                 subTitle={service.subTitle}
                 link={service.link}
