fa9dd87a7ce165f5a287ed482e83fc19714bbe19
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-22 14:22:06 +0900

    design: 높이값 변경

--- src/routes/MainHome/Home.jsx
+++ src/routes/MainHome/Home.jsx
@@ -6,14 +6,14 @@ import Menus from "./Menus/Menus";
 function Home() {
   return (
     <>
-      <div className="homeContainer">
-        <section className="flex h-screen">
+      <div className="homeContainer w-max">
+        <section className="flex w-max h-max">
           <MainView />
         </section>
-        <section className="flex h-screen">
+        <section className="flex w-max h-max">
           <PhotoZone />
         </section>
-        <section className="flex h-screen">
+        <section className="flex w-max h-max">
           <Menus />
         </section>
         <Footer />
