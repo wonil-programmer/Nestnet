a729c0cfa3e069e476f2ade027892a64b3ecbf54
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-23 23:13:32 +0900

    feat: 앨범 삭제 기능 추가

--- src/routes/Gallery/Album/Button/DeleteBtn.jsx
+++ src/routes/Gallery/Album/Button/DeleteBtn.jsx
@@ -4,6 +4,10 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 
+/**
+ * 앨범 삭제 버튼
+ * @returns
+ */
 const DeleteBtn = () => {
   const { postId } = useParams();
 
@@ -17,6 +21,7 @@ const DeleteBtn = () => {
       <Button
         onClick={handleAlbumDelete}
         content={<MdDelete className="mt-1 text-3xl" />}
+        disabled={isDeletingAlbum ? true : false}
       />
     </div>
   );
@@ -29,14 +34,17 @@ const useDeleteAlbum = () => {
 
   return useMutation({
     mutationFn: async (postId) => {
-      // const albumDeletionURL = `${process.env.REACT_APP_SERVER}/post/delete/${postId}`;
-      const albumDeletionURL = `${process.env.REACT_APP_SERVER}/photo-post?postId=${postId}`;
+      const albumDeletionURL = `${process.env.REACT_APP_SERVER}/post/delete?postId=${postId}`;
+      // const albumDeletionURL = `${process.env.REACT_APP_SERVER}/photo-post?postId=${postId}`;
       return await axios.delete(albumDeletionURL);
     },
     // 클라이언트 업데이트
     onSuccess: () => {
       queryClient.invalidateQueries(["album"]);
       navigate("/gallery");
+    },
+    onError: () => {
+      alert("앨범 삭제에 실패했습니다.");
     },
   });
 };
