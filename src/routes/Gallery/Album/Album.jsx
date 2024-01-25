f6914c958d8e23dfe92e09e07a4220f4fab7966d
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-25 14:07:34 +0900

    refactor: 앨범 메타데이터 state로 넘겨주기에서 응답데이터 사용으로 변경

--- src/routes/Gallery/Album/Album.jsx
+++ src/routes/Gallery/Album/Album.jsx
@@ -4,53 +4,47 @@ import SelectedPhoto from "./SelectedPhoto";
 import CommentRegistration from "./CommentRegistration";
 import AlbumActionBanner from "./AlbumActionBanner";
 import { useState, useEffect } from "react";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import StringCombinator from "../../../utils/Combinator/StringCombinator";
 
+/**
+ * 단건 앨범
+ * @returns
+ */
 const Album = () => {
-  const location = useLocation();
-  const { selectedPhotoPath, title, viewCount, likeCount } = location.state;
   const [selectedPhoto, setSelectedPhoto] = useState("");
-  useEffect(() => {
-    setSelectedPhoto(selectedPhotoPath);
-  }, [selectedPhotoPath]);
 
-  const [metaData, setMetadata] = useState({
-    title,
-    bodyContent: null,
-    viewCount,
-    likeCount,
-  });
+  // 앨범 댓글창(메타데이터 포함한 컴포넌트) 표시 여부
+  const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
 
   const { data = {}, isLoading: isAlbumLoading, status } = useGetAlbum();
   const {
     photoPostDto: postData,
     fileDtoList: fileData,
-    commentResponseList: commentData,
+    commentDtoList: commentData,
     memberLiked: isMemberLiked,
   } = data;
 
+  // 요청 성공시 사진 배열 첫번째 요소를 선택된 사진(현재 보고 있는 사진)으로 지정
   useEffect(() => {
     if (status === "success") {
-      setMetadata((prevState) => ({
-        ...prevState,
-        bodyContent: postData?.bodyContent,
-      }));
+      setSelectedPhoto(StringCombinator.getImagePath(fileData[0]));
     }
-  }, [status, postData?.bodyContent]);
-
-  const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
+  }, [status, fileData, postData]);
 
   return (
-    <div className="max-w-screen pt-[4.68rem] bg-home-background">
+    <div className="max-w-screen pt-[6rem] bg-home-background">
       <div className="mainView flex flex-row justify-between">
         <div className="leftSideView w-1/4 min-w-[12rem]">{/* 사이드바 */}</div>
-        <div className="centerView w-2/4 min-w-[50rem] h-[calc(100vh-4.7rem)] pl-10 overflow-y-auto">
+        <div className="centerView w-2/4 min-w-[50rem] h-[calc(100vh-6rem)] pl-10 overflow-y-auto">
           <div className="flex flex-row">
             <div className="relative flex flex-col m-auto items-center w-[40rem]">
               <div
-                className={`selectedPhotoContainer w-[40rem] h-max mt-4 ${
+                className={`selectedPhotoContainer w-[40rem] mt-4 ${
+                  selectedPhoto ? "h-max" : "h-screen"
+                } ${
                   isDescriptionVisible ? "rounded-t-3xl" : "rounded-3xl"
                 } overflow-hidden shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]`}
               >
@@ -58,15 +52,15 @@ const Album = () => {
               </div>
               <AlbumDescription
                 isAlbumLoading={isAlbumLoading}
-                metaData={metaData}
-                comments={commentData}
                 isDescriptionVisible={isDescriptionVisible}
+                postData={postData}
+                comments={commentData}
               />
               <CommentRegistration
                 isDescriptionVisible={isDescriptionVisible}
               />
             </div>
-            <div className="w-[5rem]">
+            <div className="actionBanner w-[5rem]">
               <AlbumActionBanner
                 selectedPhoto={selectedPhoto}
                 setIsDescriptionVisible={setIsDescriptionVisible}
@@ -77,7 +71,7 @@ const Album = () => {
           </div>
         </div>
         <div className="relative w-1/4">
-          <div className="w-[20rem] min-w-[20rem] max-w-[20rem] h-[calc(100vh-4.7rem)] pt-3 ml-auto px-12 pr-14 overflow-y-scroll">
+          <div className="w-[20rem] min-w-[20rem] max-w-[20rem] h-[calc(100vh-6rem)] pt-5 ml-auto px-12 pr-14 overflow-y-scroll">
             <UnselectedPhotos
               isAlbumLoading={isAlbumLoading}
               photos={fileData}
@@ -100,7 +94,7 @@ const useGetAlbum = () => {
       // const albumURL = `${process.env.REACT_APP_SERVER}/photo-post/${postId}`;
       // return await axios.get(albumURL).then((res) => res.data.response);
 
-      // test: json-server
+      // TEST: json-server
       const albumURL = `${process.env.REACT_APP_SERVER}/album/?id=${postId}`;
       return await axios.get(albumURL).then((res) => res.data[0]);
     },
