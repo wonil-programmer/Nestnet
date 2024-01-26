c4d458721efe646aef0360f40e9b90519e979d52
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-26 19:21:15 +0900

    feat: 포토존 api 테스트 완료

--- src/routes/MainHome/PhotoZone/PhotoZone.jsx
+++ src/routes/MainHome/PhotoZone/PhotoZone.jsx
@@ -1,115 +1,289 @@
-import axios from "axios";
-import { useCallback, useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { FaCloudUploadAlt } from "react-icons/fa";
-import { AiFillFile } from "react-icons/ai";
-import { MdDelete } from "react-icons/md";
-import { FcStackOfPhotos } from "react-icons/fc";
+import { MdOutlineCancel } from "react-icons/md";
+import axios from "axios";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import StringCombinator from "../../../utils/Combinator/StringCombinator";
 
 export default function PhotoZone() {
-  const [image, setImage] = useState(null);
+  const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState("No selected file");
+  const fileInputRef = useRef();
 
-  const onUploadImage = useCallback((e) => {
-    e.preventDefault();
-    if (!e.target.files) {
-      return;
+  const { data: photos = [], isLoading: isPhotosLoading } = useGetPhotos();
+  const [a, setA] = useState([]);
+  const [b, setB] = useState([]);
+  useEffect(() => {
+    if (photos.length !== 0) {
+      const halfIndex = parseInt(photos.length / 2);
+      const aSlice = photos.slice(0, halfIndex);
+      const bSlice = photos.slice(halfIndex, photos.length);
+      setA(aSlice);
+      setB(bSlice);
     }
-    const formData = new FormData();
-    formData.append("image", e.target.files[0]);
+  }, [photos]);
 
-    axios({
-      url: `http://localhost:3003/photos`,
-      method: "POST",
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-      data: formData,
-    })
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  const { mutate: createPhoto, isPending: isPhotoPending } = usePostPhoto();
+
+  // 폼 제출
+  const handlePhotoCreate = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("file", file);
+    createPhoto(formData);
+  };
 
   return (
     <section className="photoZone w-full h-fit my-10">
-      <div className="mainTitle flex flex-row items-center mb-8 px-16">
-        <span className="text-6xl">
-          <FcStackOfPhotos />
-        </span>
-        <h3 className="text-[2.5rem] font-semibold text-gray-700 mx-4 mr-6 mb-4">
+      <div className="header flex flex-row justify-between items-end mr-6 mb-8 px-16">
+        <h3 className="mainTitle text-[2.5rem] font-semibold text-gray-700 mx-4 mr-6">
           포토존
         </h3>
+        {/* 사진 업로드하는 폼 */}
+        <form
+          enctype="multipart/form-data"
+          onClick={() => {
+            fileInputRef.current.value = null;
+            fileInputRef.current.click();
+          }}
+          onSubmit={handlePhotoCreate}
+          className="h-fit"
+        >
+          <input
+            disabled={isPhotoPending}
+            ref={fileInputRef}
+            className=" hidden w-full h-full"
+            type="file"
+            accept=".jpg, .jpeg, .png"
+            onChange={({ target: { files } }) => {
+              if (files[0]) {
+                setFileName(files[0].name);
+                setFile(files[0]);
+              }
+            }}
+          />
+          <div
+            className={`${
+              file ? "bg-home-primary" : "white"
+            } flex flex-row justify-evenly items-center w-fit h-[2rem] p-2 pt-[0.6rem] 
+            font-semibold text-home-primary border-2 border-home-primary rounded-xl shadow-md hover:cursor-pointer`}
+          >
+            {file ? (
+              <>
+                <span className="mx-2 text-white">{fileName}</span>
+                <MdOutlineCancel
+                  size={18}
+                  className="text-white bg-home-primary"
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    setFileName(null);
+                    setFile(null);
+                  }}
+                />
+              </>
+            ) : (
+              <>
+                <FaCloudUploadAlt size={18} className="mr-1" />
+                <span className="text-sm">사진 올리기</span>
+              </>
+            )}
+          </div>
+          {file ? (
+            <button
+              onClick={(e) => e.stopPropagation()}
+              className="w-10 h-3 bg-home-primary"
+            >
+              올리기
+            </button>
+          ) : null}
+        </form>
       </div>
       {/* 슬라이딩 이미지 */}
-      <div className="animationList inline-block w-[300rem] animate-infiniteslide hover:[animation-play-state:paused]">
-        <div className="imgList inline-block w-[150rem]">
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b1.png"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b2.jpg"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b3.jpg"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b4.png"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b5.png"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b1.png"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b2.jpg"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b3.jpg"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b4.png"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b5.png"></img>
-          </div>
+      <div className="animationList inline-block w-[500rem] animate-infiniteslide hover:[animation-play-state:paused]">
+        <div className="imgList inline-block w-[250rem]">
+          {a
+            ? a.map((photo) => (
+                <div className="cell w-[25rem] inline-block h-fit px-1">
+                  <img
+                    className="shadow-md rounded-sm brightness-98"
+                    src={StringCombinator.getImagePath(photo)}
+                    alt={"포토존 사진"}
+                  />
+                </div>
+              ))
+            : null}
         </div>
-        <div className="imgList inline-block w-[150rem]">
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b1.png"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b2.jpg"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b3.jpg"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b4.png"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b5.png"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b1.png"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b2.jpg"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b3.jpg"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b4.png"></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img src="assets/b5.png"></img>
-          </div>
+        <div className="imgList inline-block w-[250rem]">
+          {b
+            ? b.map((photo) => (
+                <div className="cell w-[25rem] inline-block h-fit px-1">
+                  <img
+                    className="shadow-md rounded-sm brightness-98"
+                    src={StringCombinator.getImagePath(photo)}
+                    alt={"포토존 사진"}
+                  />
+                </div>
+              ))
+            : null}
         </div>
       </div>
+
+      {/* <div className="animationList inline-block w-[300rem] animate-infiniteslide hover:[animation-play-state:paused]">
+        <div className="imgList inline-block w-[150rem]">
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b2.jpg"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b3.jpg"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b4.png"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b5.png"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b1.png"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b2.jpg"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b3.jpg"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b4.png"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b5.png"
+            ></img>
+          </div>
+        </div>
+        <div className="imgList inline-block w-[150rem]">
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b1.png"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b2.jpg"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b3.jpg"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b4.png"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b5.png"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b1.png"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b2.jpg"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b3.jpg"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b4.png"
+            ></img>
+          </div>
+          <div className="cell w-[15rem] inline-block h-fit px-1">
+            <img
+              className="shadow-md rounded-sm brightness-98"
+              src="assets/b5.png"
+            ></img>
+          </div>
+        </div>
+      </div> */}
     </section>
   );
+}
+
+// REST: 포토존 사진 조회
+const useGetPhotos = () => {
+  return useQuery({
+    queryKey: ["photo-zone"],
+    queryFn: async () => {
+      const photoZoneURL = `${process.env.REACT_APP_SERVER}/life4cut?size=20`;
+
+      // TEST: json-server
+      // const photoZoneURL = `${process.env.REACT_APP_SERVER}/`;
+      return await axios.get(photoZoneURL).then((res) => {
+        return res.data.response.dtoList;
+      });
+    },
+  });
+};
+
+// REST: 사진 등록
+function usePostPhoto() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (fileFormData) => {
+      console.log(fileFormData);
+      const photoZoneURL = `${process.env.REACT_APP_SERVER}/life4cut/save`;
+      return await axios.post(photoZoneURL, fileFormData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+    },
+    // 클라이언트 업데이트
+    onSuccess: () => {
+      queryClient.invalidateQueries(["photo-zone"]);
+    },
+  });
 }
