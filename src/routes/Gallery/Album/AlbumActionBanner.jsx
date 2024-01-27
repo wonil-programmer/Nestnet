748b1243d4506d424e41f32fb248ed444733b809
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-27 14:50:13 +0900

    feat: 앨범 좋아요 기능 추가

--- src/routes/Gallery/Album/AlbumActionBanner.jsx
+++ src/routes/Gallery/Album/AlbumActionBanner.jsx
@@ -8,15 +8,16 @@ const AlbumActionBanner = ({
   selectedPhoto,
   setIsDescriptionVisible,
   isMemberLiked,
-  data: existingData,
+  likeCount,
+  data: existingAlbumData,
 }) => {
   return (
     <div className="flex flex-col flex-start items-center h-full mt-8 pr-3">
       <DownloadBtn selectedPhoto={selectedPhoto} />
       <CommentActivationBtn setIsDescriptionVisible={setIsDescriptionVisible} />
-      <LikeBtn isMemberLiked={isMemberLiked} />
+      <LikeBtn isMemberLiked={isMemberLiked} likeCount={likeCount} />
       <DeleteBtn />
-      <ModifyBtn existingData={existingData} />
+      <ModifyBtn existingData={existingAlbumData} />
     </div>
   );
 };
--- src/routes/Gallery/Album/LikeBtn.jsx
+++ src/routes/Gallery/Album/LikeBtn.jsx
@@ -1,61 +1,99 @@
 import { FaHeart } from "react-icons/fa";
-import { useContext } from "react";
 import { useParams } from "react-router-dom";
-import { IsAlbumLikeContext } from "../../../context/IsAlbumLikeContext";
-import axios from "axios";
 import { CircleButton as Button } from "../../../components/CircleButton";
+import axios from "axios";
+// import { debounce } from "lodash";
+import { useCallback, useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-const LikeBtn = ({ isMemberLiked }) => {
-  const { postId } = useParams();
+const LikeBtn = ({ isMemberLiked, likeCount }) => {
+  // const [oldLikeState, setOldLikeState] = useState(false);
+  // const [oldLikeCount, setOldLikeCount] = useState(likeCount);
 
-  const sendRequest = (url, onSuccess) => {
-    axios
-      .put(url, null, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      .then((res) => {
-        if (res.status === 200) {
-          onSuccess();
-        }
-      })
-      .catch((error) => {
-        console.error("네트워크 오류:", error);
-      });
-  };
+  const { data: isLiked } = useGetIsMemberLiked(isMemberLiked);
+  const { mutate: updateAlbumLike } = usePostAlbumLike();
 
-  // const toggleLike = () => {
-  //   if (isMemberLiked) {
-  //     const dislikeUrl = `${process.env.REACT_APP_SERVER}/${postId}/dislike`;
-  //     sendRequest(dislikeUrl, () => setIsMemberLike(!isMemberLiked));
-  //   } else {
-  //     const likeUrl = `${process.env.REACT_APP_SERVER}/${postId}/like`;
-  //     sendRequest(likeUrl, () => {
-  //       setIsMemberLike(!isMemberLiked);
-  //     });
-  //   }
-  // };
+  const queryClient = useQueryClient();
+  // const debouncedMutate = debounce(updateAlbumLike, 1000);
+
+  const handleButtonClick = useCallback(async () => {
+    await queryClient.cancelQueries({ queryKey: ["likeState"] });
+    await queryClient.cancelQueries({ queryKey: ["likeCount"] });
+
+    const previousLikeState = queryClient.getQueryData(["likeState"]);
+    const previousLikeCount = queryClient.getQueryData(["likeCount"]);
+    queryClient.setQueryData(["likeState"], !previousLikeState);
+    queryClient.setQueryData(
+      ["likeCount"],
+      previousLikeState ? previousLikeCount - 1 : previousLikeCount + 1
+    );
+    updateAlbumLike();
+    // debouncedMutate();
+  }, [updateAlbumLike, queryClient]);
 
   return (
     <div className="mt-3">
       <Button
+        onClick={() => {
+          handleButtonClick().catch((reason) => console.error(reason));
+        }}
         content={
-          isMemberLiked ? (
-            <FaHeart
-              className="m-auto text-2xl text-red-400"
-              // onClick={toggleLike}
-            />
+          isLiked ? (
+            <FaHeart className="m-auto text-2xl text-red-400" />
           ) : (
-            <FaHeart
-              className="m-auto text-2xl text-slate-300"
-              // onClick={toggleLike}
-            />
+            <FaHeart className="m-auto text-2xl text-slate-300" />
           )
         }
       />
     </div>
   );
 };
+
+// 좋아요 여부 캐시 저장
+const useGetIsMemberLiked = (isMemberLiked) => {
+  return useQuery({
+    queryKey: ["likeState"],
+    queryFn() {
+      return Promise.resolve(isMemberLiked);
+    },
+    retry: false,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    gcTime: 0,
+  });
+};
+
+// REST: 좋아요 요청
+function usePostAlbumLike() {
+  const queryClient = useQueryClient();
+  const { postId } = useParams();
+
+  return useMutation({
+    mutationFn() {
+      const likeState = queryClient.getQueryData(["likeState"]);
+      return axios.post(
+        `${process.env.REACT_APP_SERVER}/post/${
+          likeState ? "like" : "cancel-like"
+        }`,
+        { postId },
+        {
+          withCredentials: true,
+          headers: { Authorization: localStorage.getItem("access_token") },
+        }
+      );
+    },
+    // 클라이언트 업데이트
+    // SUCCESS: 재조회
+    onSuccess() {
+      return queryClient.invalidateQueries({ queryKey: ["album", postId] });
+    },
+
+    // ERROR: 기존 데이터 폴백
+    // onError() {
+    //   queryClient.setQueryData(["likeState"], oldLikeState);
+    //   queryClient.setQueryData(["likeCount"], oldLikeCount);
+    // },
+  });
+}
 
 export default LikeBtn;
