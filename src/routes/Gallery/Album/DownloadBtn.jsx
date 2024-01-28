363e22608c9081fdd7809bfd8cba3c3f69bee9fb
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-28 21:36:09 +0900

    fix: 앨범 다운로드 버튼 수정 다른 도메인에서도 다운로드 가능하게 변경

--- src/routes/Gallery/Album/DownloadBtn.jsx
+++ src/routes/Gallery/Album/DownloadBtn.jsx
@@ -1,13 +1,40 @@
 import { FiDownload } from "react-icons/fi";
 import { extractFileName } from "../../../utils/extractFileName";
 import { CircleButton as Button } from "../../../components/CircleButton";
+import axios from "axios";
 
 export default function DownloadBtn({ selectedPhoto }) {
-  const fileName = extractFileName(selectedPhoto);
+  const handleDownloadBtnClick = async () => {
+    const postId = window.location.pathname.split("/")[2];
+    const fileName = extractFileName(selectedPhoto);
+    const result = await axios
+      .get(
+        `${process.env.REACT_APP_SERVER}/file?postId=${postId}&fileName=${fileName}`,
+        {
+          responseType: "blob",
+        }
+      )
+      .then((response) => response.data);
+    const fileBlob = new Blob([result], {
+      type: "application/octet-stream",
+    });
+    const tempPhotoURL = URL.createObjectURL(fileBlob);
+    const $aElement = document.createElement("a");
+
+    $aElement.download = fileName;
+    $aElement.href = tempPhotoURL;
+    $aElement.hidden = true;
+
+    $aElement.click();
+    $aElement.remove();
+
+    URL.revokeObjectURL(tempPhotoURL);
+  };
 
   return (
-    <a download={fileName} href={selectedPhoto}>
-      <Button content={<FiDownload className="text-3xl" />} />
-    </a>
+    <Button
+      onClick={() => handleDownloadBtnClick()}
+      content={<FiDownload className="text-3xl" />}
+    />
   );
 }
