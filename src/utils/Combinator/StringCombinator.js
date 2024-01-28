254a568fe5bea87114761894543506c7977c1405
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-28 20:04:23 +0900

    refactor: 이미지 파일 경로 조합 함수 반환값 변경 문자열 템플릿 리터럴로 변경

--- src/utils/Combinator/StringCombinator.js
+++ src/utils/Combinator/StringCombinator.js
@@ -1,14 +1,13 @@
 const StringCombinator = {
+  // api 명세에 따라 이미지 경로를 조합하는 함수
   getImagePath(image) {
     const imageRootPath = `${process.env.REACT_APP_SERVER}/image`;
-    let saveFilePath = image.saveFilePath;
-    let saveFileName = image.saveFileName;
-    let imagePath = imageRootPath + "/" + saveFilePath + "/" + saveFileName;
-    // test: json-server
-    // let imagePath = "/" + saveFilePath + "/" + saveFileName;
+    // return `${imageRootPath}/${image.saveFilePath}/${image.saveFileName}`
 
-    return imagePath;
+    // test: json-server
+    return `/${image.saveFilePath}/${image.saveFileName}`;
   },
+  // 최근 글 url 경로를 조합하는 함수
   getRecentPostPath(category, postId) {
     return `${category}/${postId}`;
   },
