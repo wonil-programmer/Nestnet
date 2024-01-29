90b0bb3350a86824dd9c26cb8edc0e4740622e72
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-29 17:50:07 +0900

    fix: 최신글 없을 때 뷰 처리 추가

--- src/routes/MainHome/MainView/RecentPosts.jsx
+++ src/routes/MainHome/MainView/RecentPosts.jsx
@@ -3,17 +3,25 @@ import StringCombinator from "../../../utils/Combinator/StringCombinator";
 import { StringTranslator } from "../../../utils/Translator/StringTranslator";
 
 const RecentPosts = ({ items, isLoading }) => {
+  if (isLoading) return <></>;
+
   return (
     <div className="relative w-full h-full p-5">
-      <h1 className="mb-3 text-md font-bold text-home-primary">최신 글</h1>
+      <h1 className="mb-4 text-md font-bold text-home-primary">최신 글</h1>
       <ul className="w-full h-full text-slate-600">
-        {isLoading ? (
-          <h1>isLoading</h1>
+        {items.length === 0 ? (
+          <>
+            <h2 className="text-[0.8rem] font-slate-600">
+              최신 게시글이 없습니다.
+              <br />
+              새로운 글을 작성해보세요!
+            </h2>
+          </>
         ) : (
           items?.map((post) => (
             <li
               key={post.id}
-              className="flex flex-row items-center w-full h-[2rem] mb-[0.7rem] hover:text-black"
+              className="flex flex-row w-full h-[2rem] mb-[0.7rem] hover:text-black"
             >
               <span className="w-fit h-[1.3rem] mr-3 px-2 py-[0.2rem] text-[0.6rem] text-nowrap text-white font-semibold bg-home-primary rounded-md">
                 {StringTranslator.getPostCategoryKOR(post.postCategory)}
@@ -25,12 +33,10 @@ const RecentPosts = ({ items, isLoading }) => {
                     post.id
                   )}
                 >
-                  <div className="mb-[0.2rem] text-[0.8rem] leading-[110%] truncate">
-                    {post.title}
-                  </div>
+                  <div className="text-[0.8rem] truncate">{post.title}</div>
                 </Link>
-                <div className="text-[0.65rem] leading-[110%] truncate">
-                  {post.createdTime}
+                <div className="text-[0.65rem] truncate">
+                  {StringCombinator.getFormatDate(post.createdTime)}
                 </div>
               </div>
             </li>
