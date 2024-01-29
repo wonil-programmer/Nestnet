510fdfa1ef2ca74cf92e214e7788cf47efedf4b0
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-29 20:39:52 +0900

    feat: 날짜 조합 함수 추가

--- src/utils/Combinator/StringCombinator.js
+++ src/utils/Combinator/StringCombinator.js
@@ -2,14 +2,23 @@ const StringCombinator = {
   // api 명세에 따라 이미지 경로를 조합하는 함수
   getImagePath(image) {
     const imageRootPath = `${process.env.REACT_APP_SERVER}/image`;
-    // return `${imageRootPath}/${image.saveFilePath}/${image.saveFileName}`
+    return `${imageRootPath}/${image.saveFilePath}/${image.saveFileName}`;
 
     // test: json-server
-    return `/${image.saveFilePath}/${image.saveFileName}`;
+    // return `/${image.saveFilePath}/${image.saveFileName}`;
   },
   // 최근 글 url 경로를 조합하는 함수
   getRecentPostPath(category, postId) {
     return `${category}/${postId}`;
+  },
+  // 날짜 정보를 조합하는 함수
+  getFormatDate(createdTime) {
+    const year = createdTime[0];
+    const month = createdTime[1];
+    const day = createdTime[2];
+
+    // 날짜를 조합하여 문자열로 반환
+    return `${year}-${month}-${day}`;
   },
 };
 
