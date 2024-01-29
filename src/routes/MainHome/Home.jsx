0ee4174afd19b62bf42d095219d2dea4cfd8c3cf
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-29 23:37:48 +0900

    refactor: 포토존 스크롤 영역 내에서만 api호출로 변경

--- src/routes/MainHome/Home.jsx
+++ src/routes/MainHome/Home.jsx
@@ -2,18 +2,26 @@ import Footer from "./Footer/Footer";
 import MainView from "./MainView/MainView";
 import PhotoZone from "./PhotoZone/PhotoZone";
 import Menus from "./Menus/Menus";
+import { useInView } from "react-intersection-observer";
+import { useState, useEffect } from "react";
 
 function Home() {
+  const { ref: photoZoneRef, inView } = useInView({
+    triggerOnce: true,
+  });
+
   return (
-    <div className="overflow-hidden">
-      <div className="homeContainer w-screen bg-white">
+    <div>
+      <div className="homeContainer bg-white">
         <section className="w-full h-max px-24">
           <MainView />
         </section>
         {/* 로고 고정 배경 이미지 */}
         <section className="flex bg-fixed bg-bottom bg-no-repeat h-[9rem] bg-[url('./assets/images/bgImage.png')]"></section>
-        <section className="flex w-full h-max">
-          <PhotoZone />
+        <section className="flex w-full h-max" ref={photoZoneRef}>
+          <div className="overflow-hidden">
+            <PhotoZone inView={inView} />
+          </div>
         </section>
         <section className="flex w-full h-max px-24">
           <Menus />
