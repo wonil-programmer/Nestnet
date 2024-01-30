99828f2f5c93f440fc1be27fec0cd95ebd3dc4ad
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-30 20:51:17 +0900

    fix: 인생네컷 무한 슬라이딩 안되는 오류 수정

--- src/routes/MainHome/PhotoZone/PhotoZone.jsx
+++ src/routes/MainHome/PhotoZone/PhotoZone.jsx
@@ -1,28 +1,18 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import { FaCloudUploadAlt } from "react-icons/fa";
 import { MdOutlineCancel } from "react-icons/md";
+import { TbCameraSelfie } from "react-icons/tb";
 import axios from "axios";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import StringCombinator from "../../../utils/Combinator/StringCombinator";
 
-export default function PhotoZone() {
+export default function PhotoZone({ inView }) {
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState("No selected file");
   const fileInputRef = useRef();
 
-  const { data: photos = [], isLoading: isPhotosLoading } = useGetPhotos();
-  const [frontPhotos, setFrontPhotos] = useState([]);
-  const [rearPhotos, setRearPhotos] = useState([]);
-
-  useEffect(() => {
-    if (photos.length !== 0) {
-      const halfIdx = parseInt(photos.length / 2);
-      const frontPhotosSlice = photos.slice(0, halfIdx);
-      const rearPhotosSlice = photos.slice(halfIdx, photos.length);
-      setFrontPhotos(frontPhotosSlice);
-      setRearPhotos(rearPhotosSlice);
-    }
-  }, [photos]);
+  const { data: photos = [], isLoading: isPhotosLoading } =
+    useGetPhotos(inView);
 
   const { mutate: createPhoto, isPending: isPhotoPending } = usePostPhoto();
 
@@ -37,10 +27,7 @@ export default function PhotoZone() {
 
   return (
     <section className="photoZone w-full h-fit my-10">
-      <div className="header flex flex-row justify-between items-end mr-6 mb-8 px-16">
-        <h3 className="mainTitle text-[2.5rem] font-semibold text-gray-700 mx-4 mr-6">
-          포토존
-        </h3>
+      <div className="header flex flex-row justify-end items-end mr-6 mb-8 px-16">
         {/* 사진 업로드하는 폼 */}
         <form
           encType="multipart/form-data"
@@ -64,209 +51,88 @@ export default function PhotoZone() {
               }
             }}
           />
-          <div
-            className={`${
-              file ? "bg-home-primary" : "white"
-            } flex flex-row justify-evenly items-center w-fit h-[2rem] p-2 pt-[0.6rem] 
-            font-semibold text-home-primary border-2 border-home-primary rounded-xl shadow-md hover:cursor-pointer`}
-          >
+          <div className="flex flex-row h-fit">
+            <div
+              className={`${
+                file ? "bg-home-primary" : "white"
+              } flex flex-row justify-evenly items-center w-fit p-[0.7rem]
+            font-semibold text-home-primary border-2 border-home-primary rounded-lg shadow-md hover:cursor-pointer`}
+            >
+              {file ? (
+                <>
+                  <span className="w-fit max-w-[20rem] mx-1 text-white truncate">
+                    {fileName}
+                  </span>
+                </>
+              ) : (
+                <>
+                  <TbCameraSelfie size={24} />
+                </>
+              )}
+            </div>
             {file ? (
               <>
-                <span className="mx-2 text-white">{fileName}</span>
-                <MdOutlineCancel
-                  size={18}
-                  className="text-white bg-home-primary"
+                <button
+                  onClick={(e) => e.stopPropagation()}
+                  className="w-fit px-2 bg-white border-2 border-home-primary text-home-primary rounded-lg"
+                >
+                  <FaCloudUploadAlt size={20} className="" />
+                </button>
+                <button
+                  className="w-fit px-2 bg-home-primary text-white rounded-lg"
                   onClick={(event) => {
                     event.stopPropagation();
                     setFileName(null);
                     setFile(null);
                   }}
-                />
+                >
+                  <MdOutlineCancel size={18} />
+                </button>
               </>
-            ) : (
-              <>
-                <FaCloudUploadAlt size={18} className="mr-1" />
-                <span className="text-sm">사진 올리기</span>
-              </>
-            )}
+            ) : null}
           </div>
-          {file ? (
-            <button
-              onClick={(e) => e.stopPropagation()}
-              className="w-10 h-3 bg-home-primary"
-            >
-              올리기
-            </button>
-          ) : null}
         </form>
       </div>
       {/* 슬라이딩 이미지 */}
-      <div className="animationList inline-block w-[500rem] animate-infiniteslide hover:[animation-play-state:paused]">
-        <div className="imgList inline-block w-[250rem]">
-          {frontPhotos
-            ? frontPhotos.map((photo) => (
-                <div className="cell w-[25rem] inline-block h-fit px-1">
-                  <img
-                    className="shadow-md rounded-sm brightness-98"
-                    src={StringCombinator.getImagePath(photo)}
-                    alt={"포토존 사진"}
-                  />
-                </div>
-              ))
-            : null}
-        </div>
-        <div className="imgList inline-block w-[250rem]">
-          {rearPhotos
-            ? rearPhotos.map((photo) => (
-                <div className="cell w-[25rem] inline-block h-fit px-1">
-                  <img
-                    className="shadow-md rounded-sm brightness-98"
-                    src={StringCombinator.getImagePath(photo)}
-                    alt={"포토존 사진"}
-                  />
-                </div>
-              ))
-            : null}
-        </div>
+      <div className="animationList flex flex-row w-[400rem] animate-infiniteslide hover:[animation-play-state:paused]">
+        {photos.length === 0
+          ? null
+          : Array(2).map((_, index) => (
+              <div
+                key={index}
+                className="imgList flex flex-row items-center w-[200rem]"
+              >
+                {photos.map((photo) => (
+                  <div
+                    key={photo.id}
+                    className="cell w-[20rem] inline-block h-fit px-2"
+                  >
+                    <img
+                      className="shadow-md rounded-sm brightness-98"
+                      src={StringCombinator.getImagePath(photo)}
+                      alt={"포토존 사진"}
+                    />
+                  </div>
+                ))}
+              </div>
+            ))}
       </div>
-
-      {/* <div className="animationList inline-block w-[300rem] animate-infiniteslide hover:[animation-play-state:paused]">
-        <div className="imgList inline-block w-[150rem]">
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b2.jpg"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b3.jpg"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b4.png"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b5.png"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b1.png"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b2.jpg"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b3.jpg"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b4.png"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b5.png"
-            ></img>
-          </div>
-        </div>
-        <div className="imgList inline-block w-[150rem]">
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b1.png"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b2.jpg"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b3.jpg"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b4.png"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b5.png"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b1.png"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b2.jpg"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b3.jpg"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b4.png"
-            ></img>
-          </div>
-          <div className="cell w-[15rem] inline-block h-fit px-1">
-            <img
-              className="shadow-md rounded-sm brightness-98"
-              src="assets/b5.png"
-            ></img>
-          </div>
-        </div>
-      </div> */}
     </section>
   );
 }
 
 // REST: 포토존 사진 조회
-const useGetPhotos = () => {
+const useGetPhotos = (inView) => {
   return useQuery({
     queryKey: ["photo-zone"],
     queryFn: async () => {
       const photoZoneURL = `${process.env.REACT_APP_SERVER}/life4cut?size=20`;
-
-      // TEST: json-server
-      // const photoZoneURL = `${process.env.REACT_APP_SERVER}/`;
       return await axios.get(photoZoneURL).then((res) => {
         return res.data.response.dtoList;
       });
     },
+    gcTime: 0,
+    enabled: inView,
   });
 };
 
@@ -276,7 +142,6 @@ function usePostPhoto() {
 
   return useMutation({
     mutationFn: async (fileFormData) => {
-      console.log(fileFormData);
       const photoZoneURL = `${process.env.REACT_APP_SERVER}/life4cut/save`;
       return await axios.post(photoZoneURL, fileFormData, {
         headers: { "Content-Type": "multipart/form-data" },
