803bf492455772088bfa0e6935cfb3da17fbb857
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-30 21:40:22 +0900

    remove: 서비스 메뉴카드 컴포넌트 삭제

--- src/routes/MainHome/Menus/Menus.jsx
+++ src/routes/MainHome/Menus/Menus.jsx
@@ -1,6 +1,6 @@
-import MenuCard from "./MenuCard";
 import { useInView } from "react-intersection-observer";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Menus() {
   class Service {
@@ -19,7 +19,7 @@ export default function Menus() {
   const { ref: observeCardRef, inView } = useInView();
   const [isVisible, setIsVisible] = useState(false);
 
-  // ref가 inView 영역에 도달하면 다음 페이지를 불러옴
+  // ref가 inView 영역에 도달하면 메뉴카드를 불러옴
   useEffect(() => {
     if (inView) {
       setIsVisible(true);
@@ -31,17 +31,30 @@ export default function Menus() {
       <div className="text-[2.5rem] text-center font-semibold text-gray-700">
         서비스
       </div>
-      <div className="menuContent h-fit max-w-7xl mx-auto py-20 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-y-10 gap-x-10">
+      <div className="menuContent h-fit max-w-7xl mx-auto py-20 grid grid-rows-1 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-y-10 gap-x-10">
         {Services.map((service, idx) => (
-          <div ref={observeCardRef}>
-            <MenuCard
+          <Link to={`${service.link}`}>
+            <div
+              className={`${isVisible ? "animate-swapdown" : "opacity-0"} 
+            flex flex-col w-full h-full relative bg-white rounded-lg overflow-hidden shadow-lg  
+            duration-300 hover:ease-in-out hover:shadow-xl`}
               key={idx}
-              isVisible={isVisible}
-              imgSrc={service.imgSrc}
-              mainTitle={service.mainTitle}
-              path={service.link}
-            />
-          </div>
+              ref={observeCardRef}
+            >
+              <div className="w-full h-full overflow-hidden ease-in-out duration-300 hover:scale-110 hover:duration-300">
+                <img
+                  className="w-full h-full object-cover brightness-90"
+                  src={service.imgSrc}
+                  alt="serviceThumbnail"
+                />
+              </div>
+              <div className="absolute right-0 bottom-0 w-fit px-4 pt-2 pb-1 text-white rounded-tl-lg opacity-90">
+                <div className="mainTitle mb-2 text-white">
+                  <span className="text-xl">{service.mainTitle}</span>
+                </div>
+              </div>
+            </div>
+          </Link>
         ))}
       </div>
     </section>
