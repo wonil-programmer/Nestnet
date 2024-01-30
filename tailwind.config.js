3eab46aa493babc806f2926a4fe20e77bfedd916
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-30 20:16:49 +0900

    chore: 안쓰는 것들 정리

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,7 @@ module.exports = {
       textColor: {
         black: "#111111",
       },
-      fontSize: {
-        "home-maintitle": ["8.75rem", "10rem"],
-        "home-subtitle": "2.5rem",
-      },
+      fontSize: {},
       colors: {
         "home-primary": "#9f1239",
         "home-secondary": "#E45F84",
@@ -18,74 +15,15 @@ module.exports = {
         "default-gray": "#efefef",
         skeleton: "#d1d5db",
       },
-      width: {
-        menus: "75rem",
-        menu: "22.5rem",
-        "album-visWth": "66.25rem",
-        "album-desWth": "46.25rem",
-      },
       keyframes: {
-        fadeout: {
-          "0%": { opacity: "1" },
-          "50%": { opacity: "0.5" },
-          "75%": { opacity: "0.2" },
-          "100%": { opacity: "0", visibility: "hidden" },
-        },
         swapdown: {
           "0%": { opacity: "0", transform: "translate(0, -20%)" },
           "50%": { opacity: "0.5", transform: "translate(0, -10%)" },
           "100%": { opacity: "1", transform: "translate(0)" },
         },
-        zoomout: {
-          "0%": { transform: "scale(1.1)", opacity: "0.7" },
-          "12.5%": { transform: "scale(1.075)", opacity: "0.8" },
-          "25%": { transform: "scale(1.05)", opacity: "1" },
-          "37.5%": { transform: "scale(1.025)", opacity: "0.8" },
-          "50%": { transform: "scale(1)", opacity: "0.5" },
-          "62.5%": { transform: "scale(1)", opacity: "0.3" },
-          "75%": { transform: "scale(1)", opacity: "0" },
-          "87.5%": { transform: "scale(1.1)", opacity: "0.2" },
-          "100%": { transform: "scale(1.1)", opacity: "0.6" },
-        },
-        zoomoutnext: {
-          "0%": { transform: "scale(1.01)", opacity: "0.5" },
-          "12.5%": { transform: "scale(1)", opacity: "0.2" },
-          "25%": { transform: "scale(1)", opacity: "0" },
-          "37.5%": { transform: "scale(1.1)", opacity: "0.1" },
-          "50%": { transform: "scale(1.1)", opacity: "0.2" },
-          "62.5%": { transform: "scale(1.075)", opacity: "0.6" },
-          "75%": { transform: "scale(1.05)", opacity: "1" },
-          "87.5%": { transform: "scale(1.025)", opacity: "0.8" },
-          "100%": { transform: "scale(1.015)", opacity: "0.7" },
-        },
-        zoomin: {
-          "0%": { transform: "scale(1)" },
-          "50%": { transform: "scale(1.1)" },
-          "100%": { transform: "scale(1.2)" },
-        },
-        blink: {
-          "0%": { color: "red" },
-          "50%": { color: "blue" },
-          "100%": { color: "black" },
-        },
         wiggle: {
           "0%, 100%": { transform: "rotate(-5deg)" },
           "50%": { transform: "rotate(5deg)" },
-        },
-        // drawerout: {
-        //   "0%": { transform: "translate(0)" },
-        //   "50%": { transform: "translate(0, 50%)" },
-        //   "100%": { transform: "translate(0, 100%)" },
-        // },
-        drawerout: {
-          "0%": { bottom: "0" },
-          "50%": { bottom: "10" },
-          "100%": { bottom: "20" },
-        },
-        drawerin: {
-          "0%": { transform: "translate(0)" },
-          "50%": { transform: "translate(0, -20%)" },
-          "100%": { transform: "translate(0, -100%)" },
         },
         infiniteslide: {
           "0%": { transform: "translateX(0)" },
@@ -93,14 +31,7 @@ module.exports = {
         },
       },
       animation: {
-        fadeout: "fadeout 1s ease-out forwards",
         swapdown: "swapdown 0.5s linear forwards",
-        zoomout: "zoomout 12s linear infinite",
-        zoomoutnext: "zoomoutnext 12s linear infinite",
-        zoomin: "zoomin 5s linear infinite",
-        blink: "blink 2s ease-in-out infinite",
-        drawerout: "drawerout 0.25s linear forwards",
-        drawerin: "drawerin 0.25s linear forwards",
         infiniteslide: "infiniteslide 30s linear infinite",
       },
     },
